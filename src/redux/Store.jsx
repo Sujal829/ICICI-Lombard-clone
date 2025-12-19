d192f85d73e79006d3b60e246b4ec39868ca4272
Sujal <manishjathar01@gmail.com>
2025-12-19 23:27:28 +0530

    remove duplicate folders

--- src/redux/Store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from  '@reduxjs/toolkit'
-import UserAuth from './UserSlice'
-const Store =configureStore({
-    reducer:{
-        auth: UserAuth
-    }
-})
-
-export default Store
--- src/services/useApi.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const useApi=()=>{
-      const data = axios.get("http://localhost:5000/users")
-      .then((res)=>res.data)
-    return data
-}
-export default useApi 
